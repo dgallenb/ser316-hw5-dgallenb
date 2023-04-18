378e79d37deb75533fc90198e9828ee36a8afe18
ibstudent220 <dgallenb@asu.edu>
2023-04-17 20:24:11 -0700

    Whoops adding stat ups and explanations

--- app/src/main/java/Utility.java
+++ app/src/main/java/Utility.java
@@ -172,13 +172,13 @@ public class Utility {
     public static int[] getLevelUpBonus(MonType type, double[] scalarMods) {
         int[] totals = {0, 0, 0, 0};
         double[][] statChance = {
-                new double[] {2.5, 0.7, 0.5, 0.2}, 
-                new double[] {2.5, 0.7, 0.5, 0.35}, 
-                new double[] {2.5, 0.7, 0.5, 0.35}, 
-                new double[] {2.5, 0.7, 0.5, 0.35}, 
-                new double[] {2.5, 0.7, 0.5, 0.35}, 
-                new double[] {2.5, 0.7, 0.5, 0.35}, 
-                new double[] {2.5, 0.7, 0.5, 1.1}
+                new double[] {4.5, 0.7, 1.1, 0.2}, 
+                new double[] {2.5, 0.7, 0.6, 0.25}, 
+                new double[] {1.5, 1.2, 0.3, 0.65}, 
+                new double[] {1.2, 0.9, 0.9, 0.45}, 
+                new double[] {2.1, 0.9, 0.7, 0.25}, 
+                new double[] {2.9, 0.8, 0.8, 0.35}, 
+                new double[] {2.5, 0.9, 0.7, 1.1}
         };
         
         for(int i = 0; i < totals.length; ++ i) {
